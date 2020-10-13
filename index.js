ff7b57eecdbca6641576c92b4f7e68b9b954d02e
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2020-10-13 02:36:36 -0500

    Moving Query for Future Use

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const chalk = require("chalk");
 
 const connnection = mysql.createConnection({
     host: "localhost",
@@ -21,7 +22,7 @@ function mainPrompt() {
     inquirer.prompt({
         name: "action",
         type: "list",
-        message: "What would you like to do?",
+        message: "What Would You Like to Do?",
         choices: [
             "View All Departments",
             "View All Roles",
@@ -42,34 +43,39 @@ function mainPrompt() {
 }
 
 function onMainPromptAnswer({action}) {
-    console.log(action);
     switch(action) {
         case "View All Departments":
-            departmentSearch();
+            allDepartmentSearch();
             break;
         case "View All Roles":
-            roleSearch();
+            allRoleSearch();
             break;
         case "View All Employees":
             allEmployeeSearch();
+            break;
+        case "View All Employees by Department":
+            allEmployeeByDepartmentSearch();
+            break;
+        case "View All Employees by Manager":
+            allEmployeeByManagerSearch();
             break;
         default:
             connnection.end();
     }
 }
 
-function departmentSearch() {
-    const query = "SELECT * FROM department";
-    consoleOutQuery(query);
+const allDepartmentQuery = "SELECT * FROM department";
+function allDepartmentSearch() {
+    consoleOutQuery(allDepartmentQuery, chalk.greenBright(`Seeing All Departments!\n`));
 }
 
-function roleSearch() {
-    const query = "SELECT * FROM role";
-    consoleOutQuery(query);
+const allRoleQuery = "SELECT * FROM role";
+function allRoleSearch() {
+    consoleOutQuery(allRoleQuery, chalk.greenBright(`Seeing All Roles!\n`));
 }
 
 function allEmployeeSearch() {
-    const query = 
+    const allEmployeeQuery = 
     `SELECT 
 	    e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(e1.first_name, " ", e1.last_name) as manager 
     FROM 
@@ -77,14 +83,83 @@ function allEmployeeSearch() {
     INNER JOIN role r ON e.role_id = r.id 
     INNER JOIN department d ON r.department_id = d.id
     LEFT JOIN employee e1 ON e.manager_id = e1.id;`;
-    consoleOutQuery(query);
+    consoleOutQuery(allEmployeeQuery, chalk.greenBright(`Seeing All Employees!\n`));
 }
 
-function consoleOutQuery(query) {
-    connnection.query(query, (err, res) => {
+function allEmployeeByDepartmentSearch() {
+    const allEmployeeQueryByDeparment = 
+    `SELECT 
+	    e.first_name, e.last_name, r.title, r.salary, CONCAT(e1.first_name, " ", e1.last_name) as manager 
+    FROM 
+	    employee e
+    INNER JOIN role r ON e.role_id = r.id 
+    LEFT JOIN employee e1 ON e.manager_id = e1.id
+    WHERE r.department_id = ?`;
+    connnection.query(allDepartmentQuery, (err, allDepRes) => {
         if(err)
             throw err;
 
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "What Department Would You Like To See?",
+            choices: allDepRes
+        }).then(({department}) => {
+            let {id} = allDepRes.find(item => item.name === department);
+            connnection.query(allEmployeeQueryByDeparment, id, (err, res) => {
+                if(err)
+                    throw err;
+                console.log(chalk.greenBright(`Seeing All Employees From ${department} Department!\n`))
+                console.table(res);
+                mainPrompt();
+            })
+        });
+    });
+}
+
+const allManagersQuery = 
+`SELECT 
+    m.id, CONCAT(m.first_name, " ", m.last_name) AS name
+FROM   employee e
+JOIN   employee m on e.manager_id = m.id
+GROUP BY (e.manager_id)`;
+function allEmployeeByManagerSearch() {
+   
+    const allEmployeeByManagerQuery = 
+    `SELECT 
+	    e.first_name, e.last_name, r.title, r.salary, d.name AS department
+    FROM 
+	    employee e
+    INNER JOIN role r ON e.role_id = r.id 
+    INNER JOIN department d ON r.department_id = d.id
+    WHERE e.manager_id = ?`;
+
+    connnection.query(allManagersQuery, (err, allManagersRes) => {
+        if(err)
+            throw err;
+        inquirer.prompt({
+            name: "manager",
+            type: "list",
+            message: "What Manager's Employees Would You Like To See?",
+            choices: allManagersRes
+        }).then(({manager}) => {
+            let {id} = allManagersRes.find(item => item.name === manager);
+            connnection.query(allEmployeeByManagerQuery, id, (err, res) => {
+                if(err)
+                    throw err;
+                console.log(chalk.greenBright(`Seeing All Employees Managed by ${manager}!\n`))
+                console.table(res);
+                mainPrompt();
+            });
+        });
+    });
+}
+
+function consoleOutQuery(query, message) {
+    connnection.query(query, (err, res) => {
+        if(err)
+            throw err;
+        console.log(message);
         console.table(res);
         mainPrompt();
     });
